016920ddd76879446743bd73f119ccbdacdace60
BenPeng <asd5530025@gmail.com>
2019-12-19 02:28:39 +0800

    no message

--- main.js
+++ main.js
@@ -8,18 +8,7 @@ let channels = new Pusher('6401d26c3bbb0832b001', {
   
   // Bind a callback function to an event within the subscribed channel
   channel.bind('event-name', function(data) {
+    pushData(data)
     // Do what you wish with the data from the event
   })
-
-  async function pushData(data) {
-    const res = await fetch('/api/channels-event', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    if (!res.ok) {
-      console.error('failed to push data')
-    }
-  }
+//api/channels-event
